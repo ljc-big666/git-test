a390273216dd45864908b7ed97335d89f9c05d94
LiuChen <2075451624@qq.com>
2022-09-26 11:06:21 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName: GitTest
+ * @Description:
+ * @date: 2022/9/26 11:02
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
