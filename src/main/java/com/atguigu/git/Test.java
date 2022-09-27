36c3b9a175da72fb72927d0d4f56dfe4e4227cd1
LiuChen <2075451624@qq.com>
2022-09-27 18:06:52 +0800

    Test commit01

---
+++ src/main/java/com/atguigu/git/Test.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName: Test
+ * @Description:
+ * @date: 2022/9/27 18:05
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        int n = 123456789;
+        float f = n;
+        System.out.println(f);
+    }
+}
